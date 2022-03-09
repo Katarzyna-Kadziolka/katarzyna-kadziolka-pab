90581995caa097f717dbfd72baa8aa4dada23075
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2022-03-09 18:28:13 +0100

    lepszy kalkulator

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 var express = require('express');
 var app = express();
 var query;
-app.get('/', function (req, res) {
-    var num1 = +req.query.num1;
-    var num2 = +req.query.num2;
-    var operation = req.query.operation;
-    if (operation == 'dodawanie') {
-        var sum = num1 + num2;
-        res.send(sum.toString());
-    }
-    if (operation == 'odejmowanie') {
-        var subtraction = num1 - num2;
-        res.send(subtraction.toString());
-    }
-    if (operation == 'mnozenie') {
-        var multiplication = num1 * num2;
-        res.send(multiplication.toString());
-    }
-    if (operation == 'dzielenie') {
-        var division = num1 / num2;
-        res.send(division.toString());
-    }
+app.get('/dodaj/:num1/:num2', function (req, res) {
+    // const num1 = +req.query.num1
+    // const num2 = +req.query.num2 
+    // const operation = req.query.operation
+    var num1 = +req.params.num1;
+    var num2 = +req.params.num2;
+    var sum = num1 + num2;
+    res.send(sum.toString());
+    // if(operation == 'dodawanie') {
+    //   const sum = num1 + num2
+    //   res.send(sum.toString()) 
+    // }
+    // if(operation == 'odejmowanie') {
+    //   const subtraction = num1 - num2
+    //   res.send(subtraction.toString()) 
+    // }
+    // if(operation == 'mnozenie') {
+    //   const multiplication = num1 * num2
+    //   res.send(multiplication.toString()) 
+    // }
+    // if(operation == 'dzielenie') {
+    //   const division = num1 / num2
+    //   res.send(division.toString()) 
+    // }  
 });
 app.listen(3000);
--- index.ts
+++ index.ts
@@ -1,25 +1,36 @@
 const express = require('express')
 const app = express()
 let query
-app.get('/', function (req, res) {
-  const num1 = +req.query.num1
-  const num2 = +req.query.num2 
-  const operation = req.query.operation
-  if(operation == 'dodawanie') {
-    const sum = num1 + num2
-    res.send(sum.toString()) 
-  }
-  if(operation == 'odejmowanie') {
-    const subtraction = num1 - num2
-    res.send(subtraction.toString()) 
-  }
-  if(operation == 'mnozenie') {
-    const multiplication = num1 * num2
-    res.send(multiplication.toString()) 
-  }
-  if(operation == 'dzielenie') {
-    const division = num1 / num2
-    res.send(division.toString()) 
-  }
+app.get('/dodaj/:num1/:num2', function (req, res) {
+  // const num1 = +req.query.num1
+  // const num2 = +req.query.num2 
+  // const operation = req.query.operation
+
+  const num1 = +req.params.num1;
+  const num2 = +req.params.num2;
+  const sum = num1 + num2
+  res.send(sum.toString()) 
+
+
+
+  // if(operation == 'dodawanie') {
+  //   const sum = num1 + num2
+  //   res.send(sum.toString()) 
+  // }
+  // if(operation == 'odejmowanie') {
+  //   const subtraction = num1 - num2
+  //   res.send(subtraction.toString()) 
+  // }
+  // if(operation == 'mnozenie') {
+  //   const multiplication = num1 * num2
+  //   res.send(multiplication.toString()) 
+  // }
+  // if(operation == 'dzielenie') {
+  //   const division = num1 / num2
+  //   res.send(division.toString()) 
+  // }  
+  
+  
+
 })
 app.listen(3000)
